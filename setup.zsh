fef6358bf83f353efc243572615adec570df212e
Daniel <edblancas@gmail.com>
2014-06-26 01:31:38 -0500

    Agrega setup.zsh

---
+++ setup.zsh
@@ -0,0 +1,13 @@
+echo '--> Symlinking...' < `tty` > `tty`
+homesick link dotvim-castle < `tty` > `tty`
+
+if [ ! -d $HOME/.vim/bundle/Vundle.vim/.git/ ]
+then
+	echo '--> Cloning Vundle.vin into ~/vim/bundle...' < `tty` > `tty`
+	git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim < `tty` > `tty`
+fi
+
+echo '--> Installing vim plugins with vundle...' < `tty` > `tty`
+vim +PluginInstall! +PluginClean +qall < `tty` > `tty`
+
+echo '--> Done! Happy Viming! :x' < `tty` > `tty`
